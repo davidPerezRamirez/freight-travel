ab04f9294dc65d4772fa93d169d03d36c8e8582a
davidperezramirez <pucarauntref@gmail.com>
2019-04-05 14:08:08 -0300

    modifica url de acceso a heroku por cambio de nombre de la app

--- app/src/main/java/com/example/freight_travel/constants/ConstantsRestAPI.java
+++ app/src/main/java/com/example/freight_travel/constants/ConstantsRestAPI.java
@@ -2,7 +2,7 @@ package com.example.freight_travel.constants;
 
 public final class ConstantsRestAPI {
 
-    public static final String ROOT_URL_API_REST = "http://vast-eyrie-55341.herokuapp.com/";
+    public static final String ROOT_URL_API_REST = "http://freight-travel.herokuapp.com/";
 
     public static final String GET_ALL_DRIVERS = "getAllDrivers/";
 }
